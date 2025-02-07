dc76c4b1fb8ce5997c5794c546e58fc90fe41685
HardResetting <sribnij_d@web.de>
2025-02-07 12:13:16 +0100

    Update ValidationResult

--- src/ValidationResult.ts
+++ src/ValidationResult.ts
@@ -1,6 +1,6 @@
 export class ValidationResult {
     get valid(): boolean {
-        return (this.errors?.length ?? 0) > 0;
+        return (this.errors?.length ?? 0) == 0;
     };
     errors: string[];
 
--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,7 @@
 import EditableField from "./EditableField";
 import { HtmlExcercise } from "./HTML/HtmlExcercise";
-import { MonacoEditor, supportedLanguage } from "./MonacoEditor";
 
-const constrainedInstances: Array<typeof ConstrainedEditor> = [];
-const restrictions: Array<RangeRestriction> = [];
-
-const content = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <style>
-        .style-me {
-            /* here */
-        }
-    </style>
-</head>
-<body>
-    <div>
-        Style me! 
-        <br />
-        background-color: red;
-    </div>
-</body>
-</html>    
-`;
-
-export function test() {
-    const element = document.getElementById("container");
-    if (element == null) {
-        throw "no element";
-    }
-
-    const htmlExcercise = new HtmlExcercise(element, content);
-
-    htmlExcercise.addValidationRule
-        .required()
-        .iframeContains(".style-me", "no '.Style-me'-element!")
-        .contentIncludes("here")
-        .lambda((content, iframe) => {
-            return Math.random() > 0.5;
-        }, "Random Check failed!");
-
-
-    const styleField = new EditableField([11, 1, 11, 23], true);
-    styleField.addValidationRule
-        .required()
-        .endsWith("*/", "Need Multiline comment!");
-
-    const divField = new EditableField([16, 9, 16, 9]);
-    divField.addValidationRule
-        .required();
-
-    htmlExcercise.setEditableFields([styleField, divField]);
-
-    htmlExcercise.onValidate.on(console.log);
-
-    return htmlExcercise;
+export {
+    HtmlExcercise,
+    EditableField
 }
